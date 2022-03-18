728fbb23e63fc1a8731ab9c0bef40ead2a6d8bba
chm24k <chm24k@gmail.com>
2022-03-18 14:55:29 +0900

    playground/views say_hello()

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def say_hello(request):
+    return HttpResponse("Hello, World!")  #  request 요청하다( Client - 클라이언트 )Chrome  /  response 응답하다( Server - 서빙 )Django
+
