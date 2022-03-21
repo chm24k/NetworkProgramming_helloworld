dbcd51be9248a44fcce53b3af15659ae3a26215d
chm24k <chm24k@gmail.com>
2022-03-21 14:50:37 +0900

    views: {'name': '이름'}  ->  templates {{ name }}

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,7 @@ def say_hello(request):
     return HttpResponse("Hello, World!")  #  request 요청하다( Client - 클라이언트 )Chrome  /  response 응답하다( Server - 서빙 )Django
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name': '김미림'})
 
 def say_bye(request):
     return render(request, 'playground/bye.html')
